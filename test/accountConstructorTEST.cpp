0627160d5f16385bd9a4b16d6e2ffeb32c226428
Ben Knisley <benknisley@gmail.com>
2015-09-13 16:14:00 -0400

    Added account constructor test

---
+++ test/accountConstructorTEST.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "money.h"
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	// Create Accounts
+	account* test = new account("000000000000000");
+	return 0;
+}
